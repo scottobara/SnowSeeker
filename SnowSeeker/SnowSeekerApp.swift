ed854090d4bfd4cd8739c4aad8137a51afbcabc3
Scott Obara <scott@rubbscreations.com>
2021-03-21 17:40:46 +1100

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Scott Obara on 21/3/21.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
